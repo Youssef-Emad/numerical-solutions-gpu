726728b91f8ad6e74c11fbb9c346a0b8fb69a65b
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-09-06 17:24:00 +0200

    add jacobi one shared and local method

--- jacobi/jacobi/first_approach.cu
+++ jacobi/jacobi/first_approach.cu
@@ -56,3 +56,37 @@ __global__ void jacobiOneShared(float *x, const float *diagonal_values , const f
 	}
 }
 
+__global__ void jacobiOneSharedAndLocal(float *x, const float *diagonal_values , const float *non_diagonal_values, const int *indeces ,const float *y, const int size)
+{
+    const int index = threadIdx.x;
+	float local_diagonal_value ;
+	float local_non_diagonal_values[2];
+	int local_indeces[2];
+	float local_y;
+	__shared__ float shared_x[24];
+
+	local_diagonal_value = diagonal_values[index];
+	local_non_diagonal_values[0] = non_diagonal_values[2*index];
+	local_non_diagonal_values[1] = non_diagonal_values[2*index+1];
+	local_indeces[0] = indeces[2*index];
+	local_indeces[1] = indeces[2*index+1];
+	local_y = y[index];
+	shared_x[index] = x[index];
+
+	float sum = 0 ;
+	if (index < size)
+	{
+		for (int j = 0 ; j< 30 ; j++)
+		{
+			for (int i = 0 ; i<2 ; i++)
+			{
+				sum += local_non_diagonal_values[i]  * shared_x[local_indeces[i]] ;
+			}
+			
+			shared_x[index] = (local_y - sum )/local_diagonal_value;
+			sum = 0 ;
+			__syncthreads();	
+		}
+		x[index] = shared_x[index];
+	}
+}
