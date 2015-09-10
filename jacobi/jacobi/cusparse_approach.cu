41771de726dd189c65d571d8ad8b56a5bf99d15b
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-09-10 14:55:50 +0200

    add read from file and needed changes

--- jacobi/jacobi/cusparse_approach.cu
+++ jacobi/jacobi/cusparse_approach.cu
@@ -1,11 +1,15 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <cusparse.h>
+
+char* concat(char*,char*);
 
 __global__ void divide(float *x, float* y ,float* out ,const int size)
 {
-    const int index = threadIdx.x;
+    const int index = blockIdx.x * blockDim.x + threadIdx.x;
 
 	if (index < size)
 	{
@@ -13,20 +17,56 @@ __global__ void divide(float *x, float* y ,float* out ,const int size)
 	}
 }
 
-void jacobi_cusparse()
+void jacobi_cusparse(const int size , char* file_name)
 {
 	//initialize our test cases
-    const int m = 4;
-	const int nnz = 4 ;
+   
+	int nnz = 0 ;
 	const float alpha = -1.0;
     const float beta = 1.0;
-	float values[] = {0,0,0,0} ;
-	float diagonal_values[] = {0.1667,0.1667,0.1667,0.1667} ;
-	int rowPtr[] = {0,1,2,3,4,7};
-	int colIdx[] = {0,1,2,3};
+	float *values = (float *)malloc(2 * size * sizeof(float));
+	float *diagonal_values = (float *)malloc(size * sizeof(float));
+	int *rowPtr = (int *)malloc((size + 1) * sizeof(int));
+	int *colIdx = (int *)malloc(2 * size * sizeof(int));
+	float *x = (float *)malloc(size * sizeof(float));
+	float *y = (float *)malloc(size * sizeof(float));
+	float *output = (float *)malloc(size * sizeof(float));
 
-	float y[] = {0.2036,0.2036,0.2036,0.2036};
-	float x[] = {0,0,0,0};
+	char* diagonal_values_file_name = concat(file_name,"/csr_diagonal/csr_diagonal_value.txt") ;
+	char* non_diagonal_values_file_name = concat(file_name,"/csr_diagonal/csr_non_diagonal_value.txt");
+	char* rowPtr_file_name = concat(file_name,"/csr_diagonal/csr_rowPtr.txt");
+	char* colIdx_file_name = concat(file_name,"/csr_diagonal/csr_colIdx.txt");
+	char* y_file_name = concat(file_name,"/right_hand_side.txt");
+	char* output_file_name = concat(file_name,"/output.txt");
+
+	FILE *diagonal_values_file = fopen(diagonal_values_file_name, "r");
+	FILE *non_diagonal_values_file = fopen(non_diagonal_values_file_name, "r");
+	FILE *rowPtr_file = fopen(rowPtr_file_name, "r");
+	FILE *colIdx_file = fopen(colIdx_file_name, "r");
+	FILE *y_file = fopen(y_file_name, "r");
+	FILE *output_file = fopen(output_file_name, "r");
+
+	for (int i = 0 ; i < size ; i++)
+	{
+		fscanf(diagonal_values_file, "%f", &diagonal_values[i]);	
+		fscanf(y_file, "%f", &y[i]);	
+		fscanf(output_file, "%f", &output[i]);
+		fscanf(rowPtr_file, "%d", &rowPtr[i]);
+		x[i] = 0 ;
+		if (i == size -1 )
+		{
+			fscanf(rowPtr_file, "%d", &rowPtr[i+1]);
+		}
+	}
+
+	for (int i = 0 ; i< 2*size ; i++)
+	{
+		if (fscanf(colIdx_file, "%d", &colIdx[i]) == 1)
+		{
+			nnz++ ;
+		}
+		fscanf(non_diagonal_values_file, "%f", &values[i]);	
+	}
 
 	float *dev_values = 0 ;
 	float *dev_diagonal_values = 0 ;
@@ -44,40 +84,40 @@ void jacobi_cusparse()
     cusparseSetMatType(descr,CUSPARSE_MATRIX_TYPE_GENERAL);
     cusparseSetMatIndexBase(descr,CUSPARSE_INDEX_BASE_ZERO);
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
+     //Choose which GPU to run on, change this on a multi-GPU system.
     cudaSetDevice(0);
     
-	// Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaMalloc((void**)&dev_x, m * sizeof(float));
-	cudaMalloc((void**)&dev_y, m * sizeof(float));
+	 //Allocate GPU buffers for three vectors (two input, one output)    .
+    cudaMalloc((void**)&dev_x, size * sizeof(float));
+	cudaMalloc((void**)&dev_y, size * sizeof(float));
 	cudaMalloc((void**)&dev_values, nnz * sizeof(float));
 	cudaMalloc((void**)&dev_diagonal_values, nnz * sizeof(float));
-	cudaMalloc((void**)&dev_rowPtr, (m+1) * sizeof(int));
+	cudaMalloc((void**)&dev_rowPtr, (size + 1) * sizeof(int));
 	cudaMalloc((void**)&dev_colIdx, nnz * sizeof(int));
   
 	//Memcpy
-	cudaMemcpyAsync(dev_x, x, m * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpyAsync(dev_x, x, size * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpyAsync(dev_values, values, nnz * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpyAsync(dev_diagonal_values, diagonal_values, nnz * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpyAsync(dev_rowPtr, rowPtr, (m+1) * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpyAsync(dev_rowPtr, rowPtr, (size + 1) * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpyAsync(dev_colIdx, colIdx, nnz * sizeof(int), cudaMemcpyHostToDevice);
 
 	for (int i = 0 ; i<30 ; i++)
 	{
-		cudaMemcpyAsync(dev_y, y, m * sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpyAsync(dev_y, y, size * sizeof(float), cudaMemcpyHostToDevice);
 
 		cusparseScsrmv(handle, CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                            m, m, nnz, &alpha,
+                                            size, size, nnz, &alpha,
                                             descr, dev_values, dev_rowPtr,
                                             dev_colIdx, dev_x, &beta,dev_y);
-		divide<<<1,m>>>(dev_y,dev_diagonal_values,dev_x,m);	
+		divide<<<ceil(size/32.0),32>>>(dev_y,dev_diagonal_values,dev_x,size);	
 	}
 
-	cudaMemcpyAsync(x, dev_x, m*sizeof(float), cudaMemcpyDeviceToHost );
-	cudaMemcpyAsync(y, dev_y, m*sizeof(float), cudaMemcpyDeviceToHost );
+	cudaMemcpyAsync(x, dev_x, size*sizeof(float), cudaMemcpyDeviceToHost );
+	cudaMemcpyAsync(y, dev_y, size*sizeof(float), cudaMemcpyDeviceToHost );
 
-	// cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+	 /*cudaDeviceReset must be called before exiting in order for profiling and
+     tracing tools such as Nsight and Visual Profiler to show complete traces.*/
     cudaDeviceReset();
     cudaFree(dev_x);
     cudaFree(dev_y);
