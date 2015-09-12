e661364a20c4314e448ed7029118a064121f269b
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-09-12 17:43:51 +0200

    add write result to file

--- jacobi/jacobi/matrix_convertor_optimized.m
+++ jacobi/jacobi/matrix_convertor_optimized.m
@@ -16,17 +16,17 @@ values_in_row_count = 0 ;
 current_row_value = 1 ;
 for j = 1:size
     if result(j,1) == result(j,2)
-        diagonal(diagonal_count) = result(j,3) ;
+        diagonal_values(diagonal_count) = result(j,3) ;
         diagonal_count = diagonal_count + 1 ;
         
         if j == size && values_in_row_count < 2
-            non_diagonal(non_diagonal_count) = 0 ;
+            non_diagonal_values(non_diagonal_count) = 0 ;
             indeces(non_diagonal_count) = result(j-1,2) - 1 ;
             non_diagonal_count = non_diagonal_count + 1 ;
         end
         
     elseif result(j,1) ~= result(j,2) && result(j,1) == current_row_value && values_in_row_count < 2
-        non_diagonal(non_diagonal_count) = result(j,3) ;
+        non_diagonal_values(non_diagonal_count) = result(j,3) ;
         indeces(non_diagonal_count) = result(j,2) -1 ;
         non_diagonal_count = non_diagonal_count + 1 ;
         current_row_value = result(j,1) ;
@@ -42,7 +42,7 @@ for j = 1:size
             current_row_value = next_to_check;
         elseif next_to_check ~= current_row_value && values_in_row_count < 2
             for k = 1: 2 - values_in_row_count
-                non_diagonal(non_diagonal_count) = 0 ;
+                non_diagonal_values(non_diagonal_count) = 0 ;
                 indeces(non_diagonal_count) = result(j,2) - 1 ;
                 non_diagonal_count = non_diagonal_count + 1 ;
             end
@@ -51,3 +51,23 @@ for j = 1:size
         end
     end
 end
+
+first_approach_fileID = fopen('C:\Users\youssef\Desktop\numerical-solutions-gpu\jacobi\jacobi\test_cases\60900\first_approach\first_approach_non_diagonal_values.txt','w');
+fprintf(first_approach_fileID,'%f\n',non_diagonal_values);
+fclose(first_approach_fileID);
+
+first_approach_fileID = fopen('C:\Users\youssef\Desktop\numerical-solutions-gpu\jacobi\jacobi\test_cases\60900\first_approach\first_approach_diagonal_values.txt','w');
+fprintf(first_approach_fileID,'%f\n',diagonal_values);
+fclose(first_approach_fileID);
+
+first_approach_fileID = fopen('C:\Users\youssef\Desktop\numerical-solutions-gpu\jacobi\jacobi\test_cases\60900\first_approach\first_approach_indeces.txt','w');
+fprintf(first_approach_fileID,'%d\n',indeces);
+fclose(first_approach_fileID);
+
+output_fileID = fopen('C:\Users\youssef\Desktop\numerical-solutions-gpu\jacobi\jacobi\test_cases\60900\output.txt','w');
+fprintf(first_approach_fileID,'%f\n',E0);
+fclose(first_approach_fileID);
+
+right_hand_side_fileID = fopen('C:\Users\youssef\Desktop\numerical-solutions-gpu\jacobi\jacobi\test_cases\60900\right_hand_side.txt','w');
+fprintf(first_approach_fileID,'%d\n',rhsEE);
+fclose(first_approach_fileID);
