1408ee5062e462b1222d45544931ed2c1037395c
Ahmed Shedeed <cheetos20123@gmail.com>
2015-11-04 14:11:10 +0200

    Cleaned the code

--- gauess/kernel.cu
+++ gauess/kernel.cu
@@ -7,11 +7,7 @@
 
 char* concat(char*,char*);
 
-
-
 // Kernel
-
-
 __global__ void GaussianEliminationGlobal(const int clusterSize,float *x, const float *diagonal_values , const float *non_diagonal_values ,float *y , const int size)
 {
 	const int index = blockIdx.x * blockDim.x + threadIdx.x ;
@@ -26,18 +22,12 @@ __global__ void GaussianEliminationGlobal(const int clusterSize,float *x, const 
 			
 		}
 		matrix[i][i] = diagonal_values[i];
-		
 	}
-	
-	
-	//shared_m[0][0] = diagonal_values[0];
 	for(int i = gi; i < gi + clusterSize - 1 ;++i)
 	{
 		matrix[i][i+1] = non_diagonal_values[2*i+1];
 		matrix[i+1][i] = non_diagonal_values[2*i+2];
 	}
-
-
 
 	// triangle form
 	for (int i = gi ; i < gi + clusterSize; ++i)
@@ -54,12 +44,10 @@ __global__ void GaussianEliminationGlobal(const int clusterSize,float *x, const 
             }
             //elimination on the coefficient vector
             y[j] = y[j] - (y[i] * ratio);
-            
         }
-		  
 	}
 	__syncthreads();
-
+//Back substitution
 	for (int i = gi + clusterSize-1; i > gi-1; --i) 
     {
         double current = 0;
@@ -69,8 +57,6 @@ __global__ void GaussianEliminationGlobal(const int clusterSize,float *x, const 
         }
         x[i] = (y[i] - current) / matrix[i][i];
     }
-	
-	
 }
 
 
@@ -89,15 +75,12 @@ __global__ void GaussianEliminationShared(const int clusterSize,float *x, const 
 	for(int i = 0; i < clusterSize; ++i)
 	{
 		shared_m[i][i] = diagonal_values[clusterSize * index + i];
-		
 	}
 	for(int i = 0; i < clusterSize-1;++i)
 	{
 		shared_m[i][i+1] = non_diagonal_values[clusterSize * index * 2 + 2*i+1];
 		shared_m[i+1][i] = non_diagonal_values[clusterSize * index * 2 + 2*i+2];
 	}
-
-
 
 	// triangle form
 	for (int i = 0 ; i < clusterSize; ++i)
@@ -114,12 +97,11 @@ __global__ void GaussianEliminationShared(const int clusterSize,float *x, const 
             }
             //elimination on the coefficient vector
             y[clusterSize * index +j] = y[clusterSize * index +j] - (y[clusterSize * index +i] * ratio);
-            
         }
-		  
 	}
 	__syncthreads();
 
+	//Back substitution
 	for (int i = clusterSize-1; i > -1; --i)
     {
         double current = 0;
@@ -142,8 +124,6 @@ void gaussianCuda(const int noElem, char* file_name)
 	float *output = (float *)malloc(size * sizeof(float));
 	
 	const int clusterSize = noElem - 1;
-	
-
 	cudaEvent_t start, stop;
 	float time;
 	cudaEventCreate(&start);
@@ -164,9 +144,7 @@ void gaussianCuda(const int noElem, char* file_name)
 		fscanf(diagonal_values_file, "%f", &diagonal_values[i]);	
 		fscanf(y_file, "%f", &y[i]);	
 		x[i] = 0 ;
-		
 	}
-
 	for (int i = 0 ; i< 2*size; i++)
 	{
 		fscanf(non_diagonal_values_file, "%f", &non_diagonal_values[i]);	
@@ -176,30 +154,28 @@ void gaussianCuda(const int noElem, char* file_name)
 	float *dev_diagonal_values = 0;
     float *dev_y = 0 ;
     float *dev_x = 0;
-
-	
     // Choose which GPU to run on, change this on a multi-GPU system.
 	cudaSetDevice(0);
+
     // Allocate GPU buffers for three vectors (two input, one output)    .
 	cudaMalloc((void**)&dev_x, size * sizeof(float));
    	cudaMalloc((void**)&dev_diagonal_values, size * sizeof(float));
     cudaMalloc((void**)&dev_non_diagonal_values, 2 * size * sizeof(float));
     cudaMalloc((void**)&dev_y, size * sizeof(float));
-	
-	
+
     // Copy input vectors from host memory to GPU buffers.
 	cudaMemcpyAsync(dev_diagonal_values, diagonal_values, size * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpyAsync(dev_non_diagonal_values, non_diagonal_values, 2 * size * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpyAsync(dev_y, y, size * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpyAsync(dev_x, x, size * sizeof(float), cudaMemcpyHostToDevice);
     // Launch a kernel on the GPU with one thread for each element.
-	//const dim3 blockDim(clusterSize,1,1);
-	//const dim3 gridDim(2 * noElem,1,1);
+	const dim3 blockDim(noElem,1,1);
+	const dim3 gridDim(2 ,1,1);
 
 	cudaEventRecord(start, 0);
-
-    GaussianEliminationGlobal<<<20, 1/*,(clusterSize * clusterSize) * sizeof(float)*/>>>(clusterSize,dev_x, dev_diagonal_values , dev_non_diagonal_values , dev_y,size);
-	GaussianEliminationShared<<<20, 1/*,(clusterSize * clusterSize) * sizeof(float)*/>>>(clusterSize,dev_x, dev_diagonal_values , dev_non_diagonal_values , dev_y);
+	//uncomment the methode you want to use
+    //GaussianEliminationGlobal<<<gridDim, blockDim/*,(clusterSize * clusterSize) * sizeof(float)*/>>>(clusterSize,dev_x, dev_diagonal_values , dev_non_diagonal_values , dev_y,size);
+	//GaussianEliminationShared<<<2*noElem, 1/*,(clusterSize * clusterSize) * sizeof(float)*/>>>(clusterSize,dev_x, dev_diagonal_values , dev_non_diagonal_values , dev_y);
 		
 	//cudaEventRecord(stop, 0);
 	cudaDeviceSynchronize();
@@ -208,22 +184,14 @@ void gaussianCuda(const int noElem, char* file_name)
 	cudaEventSynchronize(stop);
 	cudaEventElapsedTime(&time, start, stop);
 	printf ("Time for the kernel: %f ms\n", time);
-
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    
     // Copy output vector from GPU buffer to host memory.
    cudaMemcpyAsync(x, dev_x, size * sizeof(float), cudaMemcpyDeviceToHost);
-  
-   
-
+	// Write the values in X array to the file out
    for (int i = 0 ; i< size ; i++)
 	{
 		fprintf(output_file, "%f",x[i]);			
 	}
-		
-
-   
+	
 	fclose(output_file);
 	free(diagonal_values);
 	free(non_diagonal_values) ;
@@ -235,8 +203,6 @@ void gaussianCuda(const int noElem, char* file_name)
     cudaDeviceReset();
     
 }
-
-
 char* concat(char *s1, char *s2)
 {
     char *result = (char *)malloc(strlen(s1)+strlen(s2)+1);
@@ -246,11 +212,7 @@ char* concat(char *s1, char *s2)
 }
 int main()
 {
-
-	
 	gaussianCuda(10,"D:\codetest");
-
-
 	// cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
     system("pause");
